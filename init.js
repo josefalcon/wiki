a04c7c0404c7190a4c0f86bc82d0cdd2fc25306f
Jose Falcon <falcon@spotify.com>
2016-08-06 16:49:19 -0400

    Make init.js

---
+++ init.js
@@ -0,0 +1,15 @@
+var elasticsearch = require('elasticsearch');
+var fs = require('fs');
+
+var host = process.env["ES_PORT_9200_TCP_ADDR"];
+var port = process.env["ES_PORT_9200_TCP_PORT"];
+
+var client = new elasticsearch.Client({
+  host: host + ':' + port
+});
+
+client.putTemplate({
+  body: fs.readFileSync('template.json').toString()
+})
+.then(r => console.log('Created index template'))
+.catch(e => console.error(e));
